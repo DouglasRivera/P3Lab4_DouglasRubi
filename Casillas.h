e8cc1ec389a59fa69b4b0c00b6f20475d9d5d76a
DouglasRivera <73321495+DouglasRivera@users.noreply.github.com>
2021-05-14 15:48:02 -0500

    Clases creadas

---
+++ Casillas.h
@@ -0,0 +1,10 @@
+#ifndef CASILLAS_H
+#define CASILLAS_H
+
+class Casillas
+{
+	public:
+	protected:
+};
+
+#endif
---
+++ Damas.h
@@ -0,0 +1,10 @@
+#ifndef DAMAS_H
+#define DAMAS_H
+
+class Damas
+{
+	public:
+	protected:
+};
+
+#endif
